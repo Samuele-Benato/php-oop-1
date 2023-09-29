da524d676caa868146dd693fdb77c32606178ede
Samuele-Benato <samuele.benato@gmail.com>
2023-09-29 15:51:53 +0200

    feat : add new movie

--- models/Movie.php
+++ models/Movie.php
@@ -3,14 +3,17 @@ class Movie
 {
     public $title;
     public $author;
+    public $year;
     public $genre;
     public $plot;
     public $duration;
     public $vote;
 
+
     public function __construct(
         string $title,
         string $author,
+        int $year,
         string $genre,
         string $plot,
         string $duration,
@@ -18,6 +21,7 @@ class Movie
     ) {
         $this->title = $title;
         $this->author = $author;
+        $this->year = $year;
         $this->genre = $genre;
         $this->plot = $plot;
         $this->duration = $duration;
